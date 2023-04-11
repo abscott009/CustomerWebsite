2f592709548b26fa36810dd06d779c1310718524
andrewscott <abscott009@gmail.com>
2023-04-11 17:53:04 -0400

    Initial commit

---
+++ src/main/java/Customer/Website/Customer/Website/CustomerWebsiteApplication.java
@@ -0,0 +1,13 @@
+package Customer.Website.Customer.Website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Customer/Website/Customer/Website/CustomerWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package Customer.Website.Customer.Website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
